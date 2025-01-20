e201316ba9699918bbf72f469954abe5cc57afc0
Neptune <karl.davalleclercq@ynov.com>
2025-01-20 14:48:11 +0100

    ok

---
+++ loader.js
@@ -0,0 +1,29 @@
+leftPanel = document.querySelector(".leftPanel")
+rightPanel = document.querySelector(".rightPanel")
+
+initButton = document.querySelector(".initButtonContainer")
+
+buttonCenter = document.querySelector(".centerInitButton")
+buttonStroke = document.querySelector(".strokeInitButton")
+
+body = document.querySelector("body")
+
+const sleepBlocking = ms => {
+  const end = Date.now() + ms;
+  while (Date.now() < end) {} // Boucle bloquante
+};
+
+
+// sleepBlocking(500); // Pause bloquante de 500 ms
+
+
+initButton.addEventListener("click", function (e) {
+  initButton.classList.add("initBounce")
+  leftPanel.classList.add("lpLeave")
+  rightPanel.classList.add("a")
+
+  body.classList.add("blackBody")
+
+})
+
+
