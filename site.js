9262c396e92d836782a775bcf6d81366a7e52cff
Neptune <karl.davalleclercq@ynov.com>
2025-01-20 14:28:51 +0100

    nodejs

--- site.js
+++ site.js
@@ -1,9 +1,26 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
+// Importation des modules nécessaires
+const express = require('express');
+const path = require('path');
+
+// Création de l'application Express
+const app = express();
+
+// Définir le port sur lequel le serveur va écouter
+const PORT = process.env.PORT || 3000;
+
+// Définir le dossier public contenant le site Web
+const publicDirectory = path.join(__dirname, 'public');
+
+// Servir les fichiers statiques du dossier public
+app.use(express.static(publicDirectory));
+
+// Route par défaut pour capturer les requêtes non gérées
+app.get('*', (req, res) => {
+    res.status(404).send('Page non trouvée');
 });
-server.listen();
+
+// Démarrer le serveur
+app.listen(PORT, () => {
+    console.log(`Le serveur est en cours d'exécution sur http://localhost:${PORT}`);
+    console.log(`Les fichiers statiques sont servis depuis le dossier: ${publicDirectory}`);
+});
