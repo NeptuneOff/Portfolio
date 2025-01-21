38258c13e6b51025ac788e2a6eddc82ed9fc9509
Neptune <karl.davalleclercq@ynov.com>
2025-01-21 11:29:19 +0100

    Choices anim

---
+++ front-js/categories.js
@@ -0,0 +1,14 @@
+webdev = document.querySelector(".webdev")
+
+robotic = document.querySelector(".robotic")
+
+choices = document.querySelectorAll(".choice")
+
+
+choices.forEach((choice) => {
+    choice.addEventListener("mouseover",function(e){
+        console.log("ok")
+    })
+});
+
+
---
+++ tests/test.js
@@ -0,0 +1,14 @@
+webdev = document.querySelector(".webdev")
+
+robotic = document.querySelector(".robotic")
+
+choices = document.querySelectorAll(".choice")
+
+
+choices.forEach((choice) => {
+    choice.addEventListener("mouseenter",function(e){
+        
+    })
+});
+
+
